72a9c345cccd9031fdedb92338e9f13b3e783c91
MohamedAshraf <mohmdashraf61@gmail.com>
2024-04-15 21:33:30 +0200

    initial commit in new repo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig , importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { firebaseConfig } from './Firebase/firebase';
+import { firebaseProviders } from './firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    firebaseProviders,
+    provideHttpClient(
+      withFetch()
+    ),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule,
+      AngularFireDatabaseModule,
+      AngularFirestoreModule
+    ])
+  ]
 };
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,143 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ChartModule } from 'angular-highcharts';
+import { Chart } from 'angular-highcharts';
+import { AddproductComponent } from '../addproduct/addproduct.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, ChartModule, AddproductComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+ 
+lineChart = new Chart({
+  accessibility: {
+    enabled: false,
+  },
+  chart: {
+    type: 'line',
+  },
+  title: {
+    text: 'Sales For The Year',
+  },
+  xAxis: {
+    categories: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+  },
+  yAxis: {},
+  credits: {
+    enabled: true,
+  },
+  plotOptions: {
+    line: {
+      dataLabels: {
+        enabled: false,
+      },
+      enableMouseTracking: true,
+    },
+  },
+  series: [
+    {
+      name: 'This Year',
+      data: [
+        10500, 50700, 40900, 15400, 35450, 10920, 18750, 21540, 20780, 78052,
+        48200, 49213,
+      ],
+    } as any,
+    {
+      name: 'Last Year',
+      data: [
+        7400, 26300, 33450, 10630, 17120, 6800, 7500, 14230, 15036, 50780,
+        30860, 25406,
+      ],
+    },
+  ],
+});
+
+  pieChart1 = new Chart({
+    chart: {
+      type: 'pie',
+    },
+    title:{
+      text: ''
+    
+    },
+    series: [
+      {
+        name: 'Percentage',
+        colorByPoint: true,
+        data: [
+          {
+            name: 'Pending',
+            y: 25.03,
+          },
+          {
+            name: 'Canceled',
+            y: 34.2,
+          },
+          {
+            name: 'Delivered',
+            y: 45,
+          },
+        ],
+      } as any,
+    ],
+  });
+  pieChart = new Chart({
+    chart: {
+      type: 'pie',
+      plotShadow: false,
+    },
+    title: {
+      verticalAlign: 'middle',
+      floating: true,
+      text: 'Products',
+    },
+    credits: {
+      enabled: false,
+    },
+    plotOptions: {
+      pie: {
+        innerSize: '99%',
+        borderWidth: 10,
+        borderColor: '',
+        slicedOffset: 10,
+        dataLabels: {
+          connectorWidth: 0,
+        },
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+    series: [
+      {
+        type: 'pie',
+        data: [
+          { name: 'Iphone 15', y: 1, color: '#eeeeee' },
+
+          { name: 'samsung', y: 2, color: '#393e46' },
+
+          { name: 'labtop', y: 3, color: '#00adb5' },
+          { name: 'clothes', y: 4, color: '#eeeeee' },
+          { name: 'shoes', y: 5, color: '#506ef9' },
+        ],
+      },
+    ],
+  });
+}
